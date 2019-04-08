13215992adab4ab3dbdb343aa00c3c08a23f0d52
Marcus Aurelius <markmayfield16@gmail.com>
2019-04-08 12:25:18 -0500

    Rendering all streams with Object.values() and .map()

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -5,16 +5,36 @@ import { fetchStreams } from '../../actions';
 class StreamList extends React.Component {
     componentDidMount() {
         this.props.fetchStreams();
+    };
+
+    renderList() {
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">{stream.description}</div>
+                    </div>
+                </div>
+            );
+        });
     }
     
     render() {
+        console.log(this.props.streams)
         return (
             <div>
-                StreamList
+                <h2>Streams</h2>
+                <div className="ui celled list">{this.renderList()}</div>
             </div>
         );
-    }
-       
+    }      
 };
 
-export default connect(null, { fetchStreams })(StreamList);
+const mapStateToProps = state => {
+    //Takes an object as a value, pulls out the values, and inserts them into an array 
+    return { streams: Object.values(state.streams) }
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);

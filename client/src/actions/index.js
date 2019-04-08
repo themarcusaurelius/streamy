543587140bb6095388cb4ac3878fbc26b4c3cf41
Marcus Aurelius <markmayfield16@gmail.com>
2019-04-08 16:05:47 -0500

    Added rederict to home page after submitted stream edit

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -39,28 +39,29 @@ export const createStream = formValues => async (dispatch, getState) => {
 export const fetchStreams = () => async dispatch => {
     const response = await streams.get('/streams');
     
-    dispatch({ type: FETCH_STREAMS, payload: response.data })
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
 //Get One Stream
 export const fetchStream = id => async dispatch => {
     const response = await streams.get(`/streams/${id}`);
 
-    dispatch({ type: FETCH_STREAM, payload: response.data })
+    dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 //Edit Stream
 export const editStream = (id, formValues) => async dispatch => {
     const response = await streams.put(`/streams/${id}`, formValues);
 
-    dispatch({ type: EDIT_STREAM, payload: response.data })
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+    history.push('/');
 };
 
 //Delete Stream
 export const deleteStream = id => async dispatch => {
     await streams.delete(`/streams/${id}`);
     
-    dispatch({ type: DELETE_STREAM, payload: id })
+    dispatch({ type: DELETE_STREAM, payload: id });
 };
 
 

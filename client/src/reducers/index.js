be37ba1ea85a0a2d97cd6c8058feb8da7f18d4b9
Marcus Aurelius <markmayfield16@gmail.com>
2019-04-08 11:51:52 -0500

    _.mapKeys on FETCH_STREAMS added

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import authReducer from './authReducers'
+import authReducer from './authReducers';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
     //replaceMe: () => 'TEST'
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    streams: streamReducer
 });
--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,17 +1,18 @@
 import _ from 'lodash';
 import {
     FETCH_STREAM, 
-    FEATCH_STREAMS,
+    FETCH_STREAMS,
     CREATE_STREAM,
     EDIT_STREAM,
     DELETE_STREAM,
-    FETCH_STREAMS,
 } from '../actions/types';
 
 
 //Object Based Approach Using Key Interpolation Syntax
 export default (state = {}, action) => {
     switch (action.type) {
+        case FETCH_STREAMS: 
+            return { ...state, ..._.mapKeys(action.payload, 'id') };
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case CREATE_STREAM:

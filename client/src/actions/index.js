ab99778c60fcdc74259f3e996824563f1f46fb6a
Marcus Aurelius <markmayfield16@gmail.com>
2019-04-08 12:31:43 -0500

    Associating streams with users with getState.auth()

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -25,8 +25,9 @@ export const signOut = () => {
 //Asynchronous Action Creator Functions
 
 //Create A New Stream
-export const createStream = formValues => async dispatch => {
-    const response = await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+    const { userId } = getState().auth
+    const response = await streams.post('/streams', { ...formValues, userId });
     
     dispatch({ type: CREATE_STREAM, payload: response.data })
 };
